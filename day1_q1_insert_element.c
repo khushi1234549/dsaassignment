516f5b774d0d5b8d9882649ac86aad34dde0ae3c
Khushi Chaturvedi <khushichaturvedi42006@gmail.com>
2026-06-29 22:32:39 +0530

    Third commit

--- day1_q1_insert_element.c
+++ day1_q1_insert_element.c
@@ -1,5 +1,5 @@
 // Day 1 - Question 1: Insert an Element in an Array
-// Close
+
 // Problem: Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
 
 // Input:
